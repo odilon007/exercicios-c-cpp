6fa90170025cecad22983065fcd57a98449fbc52
Odilon Francelino de Pontes Neto <odilonpontes7@gmail.com>
2025-11-25 10:14:34 -0300

    Create everOrNot.cpp

---
+++ c++/flowControl/everOrNot.cpp
@@ -0,0 +1,18 @@
+// using if-else statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int num;
+  cout << "Enter an integer: ";
+  cin >> num;
+
+  if (num % 2 == 0)
+    cout << num << " is even.";
+  else
+    cout << num << " is odd.";
+
+  return 0;
+}
